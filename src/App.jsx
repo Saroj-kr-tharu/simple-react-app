3a4118e5286fe09cf055ed2812918b7fbf27b69f
sarojkumartharu11234@gmail.com <104674219+Saroj-kr-tharu@users.noreply.github.com>
2025-04-06 09:21:33 +0545

    'final'

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+ 
+  return (
+   <div className="bg-slate-900 h-screen w-screen ">
+    <h2 className='text-5xl text-white  '>Hello bro </h2>
+   <span className="text-4xl block text-red-600 "> Click me just me click me  </span>
+   </div>
+  )
+}
+
+export default App
