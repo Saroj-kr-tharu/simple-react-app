637dd263bbc001fa985aaa845753172dcd985b90
sarojkumartharu11234@gmail.com <104674219+Saroj-kr-tharu@users.noreply.github.com>
2025-04-06 10:50:03 +0545

    'hool'

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ function App() {
    <div className="bg-slate-900 h-screen w-screen ">
     <h2 className='text-5xl text-white  '>Hello bro </h2>
    <span className="text-4xl block text-red-600 "> Click me just me click me  </span>
+   <span className="text-4xl block text-red-600 "> Click me just me click me  </span>
+   <span className="text-4xl block text-red-600 "> Click me just me click me  </span>
+   <span className="text-4xl block text-red-600 "> Click me just me click me  </span>
    </div>
   )
 }
